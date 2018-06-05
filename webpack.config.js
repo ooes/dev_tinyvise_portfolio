5031c5c5ad80e091df2313187fffddd4f13e6b1d
Juan Gutierrez <ooes332@gmail.com>
2018-06-05 09:56:58 -0400

    adding required files

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+ mode: "development",
+ context: path.join(__dirname, 'src'),
+ entry: [
+   './index.js',
+ ],
+ //  watch: true,
+ plugins: [
+  // new webpack.DefinePlugin({
+  //   'process.env': {
+  //     'NODE_ENV': JSON.stringify('production')
+  //   }
+  // }),
+  // // new webpack.optimize.DedupePlugin(), //dedupe similar code 
+  // new webpack.optimize.UglifyJsPlugin(), //minify everything
+  // new webpack.optimize.AggressiveMergingPlugin()//Merge chunks
+ ],
+ output: {
+   path: path.join(__dirname, 'www'),
+   filename: 'bundle.js'
+ },
+ module: {
+   rules: [
+     {
+       test: /\.js$/,
+       exclude: /node_modules/,
+       use: [
+         'babel-loader',
+       ],
+     },
+   ],
+ },
+ resolve: {
+   modules: [
+     path.join(__dirname, 'node_modules'),
+   ],
+ },
+};
